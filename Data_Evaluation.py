3fbab0bd10fbe8d74c8ea70a61bf17d89995dc27
Amlis-Sanches <nathanielchorn@nchsande.com>
2023-10-15 12:37:24 -0500

    import excel instead

--- Data_Evaluation.py
+++ Data_Evaluation.py
@@ -3,3 +3,5 @@ import numpy as np
 import pandas as pd
 import matplotlib as ptl
 
+PFAS = pd.read_excel(r'C:\Users\natha\Documents\Coding\PFASDataReview\PFAS Project Lab Known Contamination Site Tracker - May 2023 for sharing with lat_long.xlsx', sheet_name='USA Contamination Sites')
+
