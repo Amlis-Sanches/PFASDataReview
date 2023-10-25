301f8230634d674245df6cd997f61865eeb4f48d
Amlis-Sanches <nathanielchorn@nchsande.com>
2023-10-24 20:50:52 -0400

    check code

--- State_graphical_map.py
+++ State_graphical_map.py
@@ -1,9 +1,17 @@
 import pandas as pd
+import os
 import plotly.express as px
 import dash
 import dash_core_components as doc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+
+#Check if imorted file exists before reading it. 
+filename = "State_Summary.csv"
+
+if not os.path.exists(filename):
+    # If it doesn't exist, run another Python script
+    os.system("Data_Evaluation.py")
 
 df = pd.read_csv(r'C:\Users\natha\Documents\Coding\PFASDataReview\State_Summary.csv')
 
